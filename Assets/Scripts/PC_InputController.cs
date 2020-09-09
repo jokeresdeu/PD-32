72fca6b665244c335f113632d4ef64701ddc347c
DESKTOP-7RQV0BS\joker <oles.dibrivniy@tellurionmobile.com>
2020-09-09 15:03:16 +0300

    2nd lesson

---
+++ Assets/Scripts/PC_InputController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_InputController : MonoBehaviour
+{
+    [SerializeField] private Player_Movement _playerMovement;
+
+    private float _move;
+    private bool _jump;
+    private bool _crawling;
+    private void Update()
+    {
+        _move = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _jump = true;
+        }
+        _crawling = Input.GetKey(KeyCode.C);
+    }
+
+    private void FixedUpdate()
+    {
+        _playerMovement.Move(_move, _jump, _crawling);
+        _jump = false;
+    }
+}
--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -2,50 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player_Movement : MonoBehaviour
 {
     private Rigidbody2D _playerRb;
-    private SpriteRenderer _playerSprite;
-    [SerializeField] float _speed;
-    [SerializeField] float _jumpForce;
-    float _move;
 
-    // Start is called before the first frame update
+    [Header("Movement")]
+    [SerializeField] private float _speed;
+
+    [Header("Jumping")]
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _radius;
+    [SerializeField] private LayerMask _whatIsGround;
+    [SerializeField] private bool _airControll;
+    [SerializeField] private Transform _groundCheck;
+
+    [Header("Crawling")]
+    [SerializeField] private Transform _cellCheck;
+    [SerializeField] private Collider2D _headCollider;
+   
+    private float _move;
+    private bool _canJump;
+    private bool _jump;
+    private int _jumpCount;
+    private bool _crawling;
+    private bool _canStand;
+    private bool _faceRight;
 
     void Start()
     {
         _playerRb = GetComponent<Rigidbody2D>();
-        _playerSprite = GetComponent<SpriteRenderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        _move = Input.GetAxisRaw("Horizontal");
-        
-        if(Input.GetKeyUp(KeyCode.Space))
-        {
-            _playerRb.AddForce(Vector2.up * _jumpForce);
-        }
-
-        if(_move > 0 && _playerSprite.flipX)
-        {
-            _playerSprite.flipX = false;
-        }
-        else if(_move < 0 && !_playerSprite.flipX)
-        {
-            _playerSprite.flipX = true;
-        }
     }
 
     private void FixedUpdate()
     {
-        if (_move != 0)
+        if (Physics2D.OverlapCircle(_groundCheck.position, _radius, _whatIsGround))
         {
-            _playerRb.velocity = new Vector2(1, _playerRb.velocity.y) * _speed * _move;
+            _canJump = true;
+            _jumpCount = 0;
         }
         else
+            _canJump = false;
+
+        _canStand = !Physics2D.OverlapCircle(_cellCheck.position, _radius, _whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_groundCheck.position, _radius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_cellCheck.position, _radius);
+    }
+
+    public void Move(float move, bool jump, bool crawling)
+    {
+        if (_move != 0 && (_canJump || _airControll))
+        {
+            _playerRb.velocity = new Vector2(_speed * _move, _playerRb.velocity.y);
+        }
+        else if (_move == 0 && _canJump)
             _playerRb.velocity = new Vector2(0, _playerRb.velocity.y);
+
+        if(move>0 && !_faceRight)
+        {
+            Flip();
+        }
+        else if(move <0 && _faceRight)
+        {
+            Flip();
+        }
+
+        if (_jump && (_canJump || _jumpCount < 2))
+        {
+            _jumpCount++;
+            if (_jumpCount == 2)
+            {
+                _playerRb.velocity = new Vector2(_playerRb.velocity.x, 0);
+            }
+            _playerRb.AddForce(Vector2.up * _jumpForce);
+            _jump = false;
+        }
+
+        if (_crawling)
+        {
+            _headCollider.enabled = false;
+        }
+        else if (!_crawling && _canStand)
+        {
+            _headCollider.enabled = true;
+        }
+    }
+    private void Flip()
+    {
+        _faceRight = !_faceRight;
+        transform.Rotate(0, 180, 0);
     }
 }
